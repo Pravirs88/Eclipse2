1bc73feca4cb8d25bd6ab75e3d437ad314e7edf8
VandanaAndhale <nagre.vandana@gmail.com>
2022-11-23 20:18:48 +0530

    new changes

--- AllDetails.java
+++ AllDetails.java
@@ -8,6 +8,7 @@ public abstract class AllDetails {
 public void mobileNo() {
 	System.out.println("my moblie no is 8866105442");
 	System.out.println("I am from maharashtra");
+	System.out.println("Adding new branch");
 }
  abstract public void aadharNo();
 	
